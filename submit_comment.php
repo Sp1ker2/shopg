8465be145d1f1620106a513b89dc8565de03c211
Sp1ker2 <bogdan.speaker@gmail.com>
2024-11-20 09:39:29 +0200

    fix bug huina zaluonay

--- submit_comment.php
+++ submit_comment.php
@@ -2,7 +2,9 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = htmlspecialchars($_POST['name']);
     $comment = htmlspecialchars($_POST['comment']);
-    $date = date('Y-m-d H:i:s');
+
+    // Текущее время + 2 часа
+    $date = date('Y-m-d H:i:s', strtotime('+2 hours'));
     $file = 'comments.txt';
 
     // Добавляем строку с датой, именем и комментарием
